28f17d347e1306249bd0c2f881aca74fafdb6ae6
Egor Sotnikov <web.abudabi@gmail.com>
2024-04-24 16:14:56 -0500

    added js logic for counter updating

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,17 +1,21 @@
 import { getLocalStorage } from "./utils.mjs";
 
-let total = 0;
+const cartLSKey = "so-cart";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart") || [];
+  const productList = document.querySelector(".cart-page .product-list");
+  if (!productList) return;
+
+  const cartItems = getLocalStorage(cartLSKey) || [];
   if (cartItems.length === 0) {
-    document.querySelector(".product-list").innerHTML =
+    productList.innerHTML =
       `<p class="empty-cart-message">There are currently no items in your cart</p>`;
     document.querySelector(".cart-total").style.display = "none";
   } else {
     const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-    document.querySelector(".product-list").innerHTML = htmlItems.join("");
+    productList.innerHTML = htmlItems.join("");
 
+    let total = 0;
     for (let i = 0; i < cartItems.length; i++) {
       total = total + cartItems[i].quantity * cartItems[i].FinalPrice;
     }
@@ -39,6 +43,21 @@ function cartItemTemplate(item) {
 </li>`;
 
   return newItem;
+}
+
+export function updateCartCounter(cart = null) {
+  const cartCounter = document.querySelector(".cart-counter");
+  if (!cartCounter) return;
+
+  const cartItems = cart || getLocalStorage(cartLSKey) || [];
+
+  if (!Array.isArray(cartItems) || cartItems.length === 0) {
+    cartCounter.innerHTML = 0;
+    cartCounter.style.display = "none";
+  } else {
+    cartCounter.innerHTML = cartItems.reduce((total, current) => total + current.quantity, 0);
+    cartCounter.style.display = "block";
+  }
 }
 
 renderCartContents();
