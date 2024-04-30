d81feee29497bf84cc7f1f7c097bfc2b9ae9680f
Egor Sotnikov <web.abudabi@gmail.com>
2024-04-30 14:55:22 -0500

    added alerts modal window

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,16 @@
 import { updateCartCounter } from "./cart";
+import Alert from "./Alert";
+
+const homePageBody = document.querySelector(".home-page");
 
 updateCartCounter();
+showAlerts();
+
+
+function showAlerts() {
+  // only on home page
+  if (!homePageBody) return;
+
+  const alert = new Alert("../json/alerts.json");
+  alert.show(homePageBody);
+}
